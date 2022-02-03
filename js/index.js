20b7ef1c1b2982a5b83ff8a3782951c5ecea13e9
Rita Cunha <rita.cunha.ts@gmail.com>
2022-02-03 10:23:56 +0000

    fixed canvas display

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ window.onload = () => {
         startGame();
     };
     function startGame() {
+        document.getElementById("game-board").style.display = "flex"
         document.getElementById("intro").remove();
         document.getElementById("canvas").style.background = "url('./docs/assets/images/uv_map_imagebackground50.png')";
         const game = new Game();
